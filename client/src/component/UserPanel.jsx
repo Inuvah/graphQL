0cc9d523c9d68c971b8f6ae8f6275ffca3197742
Inovah <oliverpetersenmandalia@gmail.com>
2024-12-18 10:11:29 +0100

    Update UserPanel.jsx

--- client/src/component/UserPanel.jsx
+++ client/src/component/UserPanel.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 export const UserPanel = (props) => {
   return (
-    
+
     <div className='flex-center-row lightGray'>
         <button onClick={()=>{props.setShowLogin("show")}}>Login</button>
+        <button onClick={()=>{props.setShowRegister("show")}}>Register</button>
         <p>{props.userName}</p>
     </div>
   )
